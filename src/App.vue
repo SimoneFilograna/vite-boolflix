9a959b6cc6afd96788c1acc916580c9eaa08d77f
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-28 15:36:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+  import TheHeader from "../src/components/TheHeader.vue";
+  import TheFooter from "./components/TheFooter.vue";
+
+  export default{
+    components:{
+      TheHeader,
+      TheFooter
+    }
+  }
+</script>
+
+<template>
+
+  <TheHeader></TheHeader>
+
+  <main></main>
+
+  <TheFooter></TheFooter>
+</template>
+
+<style lang="scss">
+</style>
