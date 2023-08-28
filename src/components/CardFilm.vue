17b6db09e377915a8b5c8dd6ee005f91313a0c4f
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-28 19:07:48 +0200

    fixed-card

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -12,11 +12,11 @@
 <template>
     <div class="card h-100">
         <img src="" class="card-img-top" alt="">
-        <div class="card-body text-center">
-            <h5 class="card-title">{{objfilm.title}}</h5>
-            <p class="card-text">{{objfilm.original_title}}</p>
-            <p class="card-text">{{objfilm.original_language}}</p>
-            <p class="card-text">{{objfilm.vote_average}}</p>
+        <div class="card-body">
+            <h5 class="card-title">Title: {{objfilm.title}}</h5>
+            <p class="card-text">Original title: {{objfilm.original_title}}</p>
+            <p class="card-text">Original language: {{objfilm.original_language}}</p>
+            <p class="card-text">Vote: {{objfilm.vote_average}}</p>
         </div>
     </div>
 </template>
