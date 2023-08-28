276d57d354444240c024be1a60ee6d652e0fda39
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-28 19:32:06 +0200

    flag-implement

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,10 +1,14 @@
 <script>
+    import CountryFlag from 'vue-country-flag-next'
     export default{
         props:{
             objfilm: {
                 type: Object,
                 required: true
             }
+        },
+        components:{
+            CountryFlag
         }
     }
 </script>
@@ -12,11 +16,11 @@
 <template>
     <div class="card h-100">
         <img src="" class="card-img-top" alt="">
-        <div class="card-body">
+        <div class="card-body d-flex flex-column">
             <h5 class="card-title">Title: {{objfilm.title}}</h5>
             <p class="card-text">Original title: {{objfilm.original_title}}</p>
-            <p class="card-text">Original language: {{objfilm.original_language}}</p>
-            <p class="card-text">Vote: {{objfilm.vote_average}}</p>
+            <country-flag :country='objfilm.original_language' size='small' class="mt-auto"/>
+            <p class="card-text pt-2">Vote: {{objfilm.vote_average}}</p>
         </div>
     </div>
 </template>
