0e38b43cff46b66ca926d8d638016a713504a5f3
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-29 12:05:42 +0200

    img-filmcard

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -15,7 +15,7 @@
 
 <template>
     <div class="card h-100">
-        <img src="" class="card-img-top" alt="">
+        <img :src="`https://image.tmdb.org/t/p/w342/${objfilm.poster_path}`" class="card-img-top" alt="">
         <div class="card-body d-flex flex-column">
             <h5 class="card-title">Title: {{objfilm.title}}</h5>
             <p class="card-text">Original title: {{objfilm.original_title}}</p>
